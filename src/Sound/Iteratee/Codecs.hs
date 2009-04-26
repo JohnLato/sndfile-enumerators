95e3a6ae459618e42f1e39c6a65582837c0dd638
jwlato <jwlato@gmail.com>
2009-04-26 01:30:56 +0000

    add getWriter function

--- src/Sound/Iteratee/Codecs.hs
+++ src/Sound/Iteratee/Codecs.hs
@@ -1,10 +1,22 @@
 module Sound.Iteratee.Codecs (
   module Sound.Iteratee.Codecs.SupportedFormats,
-  module Sound.Iteratee.Codecs.Wave
+  module Sound.Iteratee.Codecs.Wave,
+  getWriter
 )
 
 where
 
+import Sound.Iteratee.Base
 import Sound.Iteratee.Codecs.Wave
 import Sound.Iteratee.Codecs.SupportedFormats
+import Data.Iteratee
+
+import qualified Data.StorableVector as SV
+
+getWriter :: SupportedFileFormat ->
+             FilePath ->
+             AudioFormat ->
+             IterateeGM SV.Vector Double AudioMonad ()
+getWriter Wave = writeWave
+getWriter Raw  = error "No writer defined for Raw format"
 
