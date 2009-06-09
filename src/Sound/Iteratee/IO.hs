d0390ec0761f616e51f3e500a1ec26c3cdb88ed9
jwlato <jwlato@gmail.com>
2009-06-09 22:54:55 +0000

    improve formatting of IO.hs

--- src/Sound/Iteratee/IO.hs
+++ src/Sound/Iteratee/IO.hs
@@ -47,28 +47,28 @@ enumAudioFile h iter = get >>= \st ->
           IO (IterateeG s el AudioMonad a)
   loop _sst (off,len) _iter' _p | off `seq` len `seq` False = undefined
   loop sst (off,len) iter' p = do
-   n <- (try $ hGetBuf h p buffer_size) :: IO (Either SomeException Int)
-   case n of
-    Left _errno -> evalStateT (enumErr "IO error" iter') sst
-    Right 0 -> return iter'
-    Right n' -> do
+    n <- (try $ hGetBuf h p buffer_size) :: IO (Either SomeException Int)
+    case n of
+      Left _errno -> evalStateT (enumErr "IO error" iter') sst
+      Right 0 -> return iter'
+      Right n' -> do
          s <- readFromPtr p (fromIntegral n')
          (igv, sst') <- runStateT (runIter iter' (Chunk s)) sst
-	 check sst' (off + fromIntegral len,fromIntegral n') igv p
+         check sst' (off + fromIntegral len,fromIntegral n') igv p
   seekTo sst pos@(off,len) off' iter' p
     | off <= off' && off' < off + fromIntegral len =	-- Seek within buffer
     do
     let local_off = fromIntegral $ off' - off
-    str <- readFromPtr (p `plusPtr` local_off) (len - local_off)
+    str      <- readFromPtr (p `plusPtr` local_off) (len - local_off)
     (igv, s) <- runStateT (runIter iter' (Chunk str)) sst
     check s pos igv p
   seekTo sst _pos off iter' p = do		-- Seek outside the buffer
     off' <- (try $ hSeek h AbsoluteSeek
-             (fromIntegral off)) :: IO (Either SomeException ())
+            (fromIntegral off)) :: IO (Either SomeException ())
     case off' of
-      Left _errno -> evalStateT (enumErr "IO error" iter') sst
-      Right _     -> loop sst (off, 0) iter' p
-  check _   _ (Done x _)                 _ = return . return $ x
+      Left  _errno -> evalStateT (enumErr "IO error" iter') sst
+      Right _      -> loop sst (off, 0) iter' p
+  check sst   _ (Done x _)                 _ = return $ lift (put sst >> (return x))
   check sst o (Cont k Nothing)           p = loop sst o k p
   check sst o (Cont k (Just (Seek off))) p = seekTo sst o off k p
   check _   _ (Cont _ (Just e))          _ = return $ throwErr e
