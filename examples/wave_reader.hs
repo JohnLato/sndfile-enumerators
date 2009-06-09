450839c8e83d67a4ac79a71fc6027c5d29ddc10a
jwlato <jwlato@gmail.com>
2009-06-09 22:53:04 +0000

    update examples

--- examples/wave_reader.hs
+++ examples/wave_reader.hs
@@ -20,31 +20,33 @@ main = do
     [] -> putStrLn "Usage: wave_reader FileName"
     fname:xs -> do
       putStrLn $ "Reading file: " ++ fname
-      fileDriverRandom (wave_reader >>= test) fname
+      fileDriverRandom (waveReader >>= test) fname
       return ()
 
--- Use the collection of [WAVEDE] returned from wave_reader to
+-- Use the collection of [WAVEDE] returned from waveReader to
 -- do further processing.  The IntMap has an entry for each type of chunk
 -- in the wave file.  Read the first format chunk and disply the 
--- format information, then use the dict_process_data function
+-- format information, then use the dictProcessData function
 -- to enumerate over the max_iter iteratee to find the maximum value
 -- (peak amplitude) in the file.
-test :: Maybe (IM.IntMap [WAVEDE]) -> IterateeGM V Word8 IO ()
+test :: Maybe (IM.IntMap [WAVEDE]) -> IterateeG V Word8 IO ()
 test Nothing = lift $ putStrLn "No dictionary"
 test (Just dict) = do
-  fmtm <- dict_read_first_format dict
+  fmtm <- dictReadFirstFormat dict
   lift . putStrLn $ show fmtm
-  maxm <- dict_process_data 0 dict max_iter
+  maxm <- dictProcessData 0 dict max_iter
   lift . putStrLn $ show maxm
   return ()
 
 -- an iteratee that calculates the maximum value found so far.
 -- this could be written with snext as well, however it is more
 -- efficient to operate on an entire chunk at once.
-max_iter :: IterateeGM V Double IO Double
+max_iter :: IterateeG V Double IO Double
 max_iter = m' 0
   where
-  m' acc = liftI $ Cont (step acc)
-  step acc (Chunk xs) | SV.null xs = m' acc
-  step acc (Chunk xs)              = m' $! SV.foldl' (max . abs) acc xs
-  step acc str = liftI $ Done acc str
+  m' acc = IterateeG (step acc)
+  step acc (Chunk xs) | SV.null xs = return $ Cont (m' acc) Nothing
+  step acc (Chunk xs) = return $ Cont
+                        (m' $! SV.foldl' (max . abs) acc xs)
+                        Nothing
+  step acc str = return $ Done acc str
--- examples/wave_writer.hs
+++ examples/wave_writer.hs
@@ -20,30 +20,30 @@ main = do
   case args of
     fname:oname:xs -> do
       putStrLn $ "Reading file: " ++ fname
-      fileDriverAudio (wave_reader >>= writer oname) fname
-      --runAudioMonad $ unIM $ (writer2 oname)
+      fileDriverAudio (waveReader >>= writer oname) fname
+      --runAudioMonad $ run $ writer2 oname
       return ()
     _ -> putStrLn "Usage: wave_writer ReadFile WriteFile"
 
--- Use the collection of [WAVEDE] returned from wave_reader to
+-- Use the collection of [WAVEDE] returned from waveReader to
 -- do further processing.  The IntMap has an entry for each type of chunk
 -- in the wave file.  Read the first format chunk and disply the 
--- format information, then use the dict_process_data function
+-- format information, then use the dictProcessData function
 -- to enumerate over the max_iter iteratee to find the maximum value
 -- (peak amplitude) in the file.
-writer :: FilePath -> Maybe (IM.IntMap [WAVEDE]) -> IterateeGM V Word8 AudioMonad ()
+writer :: FilePath -> Maybe (IM.IntMap [WAVEDE]) -> IterateeG V Word8 AudioMonad ()
 writer _ Nothing = liftIO $ putStrLn "No dictionary"
 writer fp (Just dict) = do
-  fmtm <- dict_read_first_format dict
+  fmtm <- dictReadFirstFormat dict
   liftIO $ putStrLn $ show fmtm
   maybe (error "No format")
-        (\fmt -> dict_process_data 0 dict $ writeWave fp fmt)
+        (\fmt -> dictProcessData 0 dict $ writeWave fp fmt)
         fmtm
   return ()
 
 -- |Write an empty wave file to the specified file
-writer2 :: FilePath -> IterateeGM V Double AudioMonad ()
+writer2 :: FilePath -> IterateeG V Double AudioMonad ()
 writer2 fp = do
   let fmt = AudioFormat 2 44100 16
-  enumPure1Chunk SV.empty ==<< writeWave fp fmt
+  joinIM $ enumPure1Chunk SV.empty $ writeWave fp fmt
   return ()
