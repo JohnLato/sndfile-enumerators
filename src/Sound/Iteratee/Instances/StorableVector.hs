49aa3506584d898748fcf60de83dbaf4785fe163
jwlato <jwlato@gmail.com>
2009-06-23 14:50:07 +0000

    bugfix: ReadableChunk instance for StorableVector Previously the 'length' parameter was parsed improperly, leading to bugs when the word size was greater than 1 byte.  This should behave now.

--- src/Sound/Iteratee/Instances/StorableVector.hs
+++ src/Sound/Iteratee/Instances/StorableVector.hs
@@ -13,7 +13,6 @@ import Data.Monoid
 import qualified Data.ListLike as LL
 import Foreign.Storable
 import Foreign.ForeignPtr
-import Foreign.Marshal.Array
 
 instance (Storable el) => LL.ListLike (SV.Vector el) el where
   length    = SV.length
