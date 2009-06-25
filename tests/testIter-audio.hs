f879ebeabf6edef6b97f6db403dc1110e869fa16
jwlato <jwlato@gmail.com>
2009-06-25 18:34:37 +0000

    add dependency to word24 The Int24 and Word24 files have been pulled and placed into the word24 package.

--- tests/testIter-audio.hs
+++ tests/testIter-audio.hs
@@ -13,46 +13,10 @@ import Data.Word
 import Data.Word.Word24
 
 -- ----------------------------------------
--- Word24 Properties
-
-prop_addIdent a = a - a == 0
-  where types = a :: Word24
-
-prop_multIdent a = a * 1 == a
-  where types = a :: Word24
-
-prop_unsigned a = a >= 0
-  where types = a :: Word24
-
-prop_smaller a = (fromIntegral ((fromIntegral a) :: Word24) :: Word16) == a
-
-
--- ----------------------------------------
--- Int24 Properties
-
-prop_addIdentI a = a - a == 0
-  where types = a :: Int24
-
-prop_multIdentI a = a * 1 == a
-  where types = a :: Int24
-
-prop_smallerI a = (fromIntegral ((fromIntegral a) :: Int24) :: Int16) == a
-
 
 -- ----------------------------------------
 -- tests
 tests = [
-  testGroup "Word24" [
-    testProperty "add. identity" prop_addIdent
-    ,testProperty "mult. identity" prop_multIdent
-    ,testProperty "unsigned" prop_unsigned
-    ,testProperty "Word16/Word24 conversion" prop_smaller
-    ]
-  ,testGroup "Int24" [
-    testProperty "add. identity" prop_addIdentI
-    ,testProperty "mult. identity" prop_multIdentI
-    ,testProperty "Int16/Int24 conversion" prop_smallerI
-    ]
   ]
 
 -- ----------------------------------------
