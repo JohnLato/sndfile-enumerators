2dbedb14be7415462b319927111dba53ecae4bb7
jwlato <jwlato@gmail.com>
2010-05-04 09:01:04 +0000

    remove Channelized code

--- src/Sound/Iteratee.hs
+++ src/Sound/Iteratee.hs
@@ -1,6 +1,5 @@
 module Sound.Iteratee (
   module Sound.Iteratee.Base,
-  module Sound.Iteratee.Channelized,
   module Sound.Iteratee.Writer,
   module Sound.Iteratee.Codecs
 )
@@ -8,7 +7,6 @@ module Sound.Iteratee (
 where
 
 import Sound.Iteratee.Base
-import Sound.Iteratee.Channelized
 import Sound.Iteratee.Codecs
 import Sound.Iteratee.Writer
 
