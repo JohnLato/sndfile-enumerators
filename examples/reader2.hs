62a68e067580cd3ce1ae6a575121e44858f0c1e8
John Lato <jwlato@tsurucapital.com>
2014-03-20 16:45:24 +0900

    remove cruft from reader2.hs

--- examples/reader2.hs
+++ examples/reader2.hs
@@ -26,29 +26,12 @@ main = do
             genAudio fname t1
       print e
 
-{-# RULES
--- this rule seems to hurt, needs a little more investigation?
--- "<iterx>maps/foldingM" forall f g s. maps f (foldingM g s) = foldingM (\b -> g b . f) s
-"<iterx>maps/maps_2" forall x f g. maps f (maps g x) = maps (g . f) x
-      #-}
-
 {-# INLINE t1 #-}
--- the ForceSpecConstr in foldUnfolding helps a lot, but it's still
--- 25% slower than the vector version.
---
--- And the core looks really nice, but Unfold can be improved.
---
--- foldFoldable is slow, because the inner loop isn't unboxed?  WTF?
+--  This is now as fast as the vector code, yay!  But I'm not
+--  entirely happy with the unfolding/Identity/Monad story.
+--  but maybe I can leave that be for now...
 t1 :: FoldM AudioMonad NormFormattedChunk Double
--- t1 = maps nfChunkData . foldUnfolding2 unfold2Vec . maps abs $ folding max 0
--- t1 = maps nfChunkData . maps unfold2Vec . foldUnfolding2a . maps abs $ folding max 0
-
--- this is nearly as fast as the plain vector code.  removing the
--- `runIdentity` makes it a bit slower, but it's still pretty fast.
--- t1 = maps nfChunkData . maps (\v -> runIdentity $ stepFold (foldUnfolding2 unfold2Vec . maps abs $ folding max 0) v >>= getFold) $ folding max 0
--- t1 = maps nfChunkData . unfolding unfold2Vec . maps abs $ folding max 0
 t1 = maps nfChunkData . unfolding unfold2Vec . maps abs $ folding max 0
--- t1 = maps nfChunkData . foldUnfolding unfoldVec $ folding maxf1 0
 -- t1 = maps nfChunkData . maps (V.maximum . V.map abs) $ folding max 0
 
 maxf1 :: Double -> Double -> Double
